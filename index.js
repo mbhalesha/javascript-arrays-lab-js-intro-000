f6a065f1f7431b18e69bb058f42fe7dea258eaac
Manisha Bhalesha <mbhalesha@gmail.com>
2018-06-13 20:48:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,11 @@ function destructivelyRemoveFirstKitten() {
   return kittens;
 }
 
-function appendKitten(name){
+function appendKitten(name) {
   return[...kittens, name];
 }
+
+function prependKitten(name) {
+  return[name,...kittens];
+}
+
